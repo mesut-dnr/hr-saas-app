182dbbc47210bc9c9971f7a641145b263a4457a8
mesut-dnr <mesutdonerng@gmail.com>
2023-08-06 13:06:33 +0300

    implementation added

---
+++ department-services/infrastructure/src/main/java/com/hr/saas/app/DepartmentApprovalRequestMessageListenerImpl.java
@@ -0,0 +1,16 @@
+package com.hr.saas.app;
+
+import com.hr.saas.app.dto.DepartmentApprovalRequest;
+import com.hr.saas.app.port.input.message.listener.employee.DepartmentApprovalRequestMessageListener;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class DepartmentApprovalRequestMessageListenerImpl implements DepartmentApprovalRequestMessageListener {
+    @Override
+    public void approveEmployee(DepartmentApprovalRequest departmentApprovalRequest) {
+
+    }
+}
